58e4d0d63758814c45d9ce9fe9704c94410ce76a
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-10 18:19:02 -0300

    itens pedido tabela relacionamento

---
+++ tables/itens_pedido.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ItensPedido (
+    ItemID INT PRIMARY KEY AUTO_INCREMENT,
+    PedidoID INT,
+    ProdutoID INT,
+    Quantidade INT NOT NULL,
+    PrecoUnitario DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (PedidoID) REFERENCES Pedidos(PedidoID),
+    FOREIGN KEY (ProdutoID) REFERENCES Produtos(ProdutoID)
+);
